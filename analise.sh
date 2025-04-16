8028c603ceb7d9b79586146735781e1f18d5a3ff
Andersoney Rodrigues <andersoney.rodrigues@prodeb.ba.gov.br>
2025-04-16 07:03:22 -0300

    melhorando a saida

--- analise.sh
+++ analise.sh
@@ -26,7 +26,7 @@ do
         mkdir -p "saida/cout/nRobots$nRobos"
         
         echo "$nRobos $i - Executando para $nRobos $i pois o arquivo saidas/nRobots$nRobos/log\_$i n existe"
-        ./testar.sh $nRobos $i -gui &>>"saida/cout/nRobots$nRobos" &
+        ./testar.sh $nRobos $i -gui &>>"saida/cout/nRobots$nRobos/logs_$i.log" &
         
     done
     echo "Todas as execuções finalizaram ${nRobos}"
